0fe1fedb4db62908eb1ba6910e60b3e136d3bdf8
Juanita <juanitapechhernandez38@gmail.com>
2021-04-25 02:26:59 -0500

    Django

--- Tiendaonline/gestionpedidos/views.py
+++ Tiendaonline/gestionpedidos/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from gestionpedidos.models import Articulos
+from django.core.mail import send_mail
+from django.conf import settings
+from gestionpedidos.forms import FormularioContacto
 
 # Create your views here.
+
+def busqueda_productos(request):
+
+    return render(request, "busqueda_productos.html")
+
+
+def buscar(request):
+
+
+    if request.GET["prd"]:
+
+        #mensaje="Articulo buscado: %r" %request.GET["prd"]
+        producto=request.GET["prd"]
+
+        if len(producto)>20:
+
+            mensaje="Texto de busqueda demasiado largo"
+
+        else:
+
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
+            
+            return render(request, "resultados_busqueda.html", {"articulos" : articulos, "query" : producto})
+
+    else:
+
+        mensaje="No has introducido nada"
+    
+    return HttpResponse(mensaje)
+
+
+def contacto(request):
+
+    if request.method=="POST":
+
+
+        miFormulario=FormularioContacto(request.POST)
+
+        if miFormulario.is_valid():
+
+            infForm=miFormulario.cleaned_data
+
+            send_mail(infForm['asunto'], infForm['mensaje'],
+            infForm.get('email',''),['arielleal2206@gmail.com'],)
+
+            return render(request, "gracias.html")
+
+           #subject=request.POST["asunto"]
+
+           #message=request.POST["mensaje"] + " " + request.POST["email"]
+
+           #email_from=settings.EMAIL_HOST_USER
+  
+           #recipient_list=["arielleal2206@gmail.com"]
+
+           #send_mail(subject, message, email_from, recipient_list)
+
+    else:
+
+        miFormulario=FormularioContacto()
+
+        
+    return render(request, "formulario_contacto.html", {"form" : miFormulario})
+
+        #return render(request, "gracias.html")        
+
+    #return render(request, "contacto.html")
+    
+
+
